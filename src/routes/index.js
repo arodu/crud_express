d955b1f2650721cbf52a8e3d7e9da412a5b90619
Alberto Rodriguez <arodu.dev@gmail.com>
2019-07-11 17:48:47 -0400

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,45 @@
 const express = require('express')
 const router = express.Router()
 
+const Task = require('../models/task')
 
+router.get('/', async (req, res) => {
+  const tasks = await Task.find()
+  //console.log(tasks)
+  res.render('index', {
+    tasks: tasks
+  })
+})
+
+router.post('/add', async (req, res) => {
+  const task = new Task(req.body)
+  await task.save()
+  //res.send('recived')
+
+  //res.render('index')
+  //console.log(req.body)
+
+  res.redirect('/')
+})
+
+router.get('/edit/:id', async (req, res) => {
+  const { id } = req.params
+  const task = await Task.findById(id)
+
+})
+
+router.get('/turn/:id', async (req, res) => {
+  const { id } = req.params
+  const task = await Task.findById(id)
+  task.status = !task.status
+  await task.save()
+  res.redirect('/')
+})
+
+router.get('/delete/:id', async (req, res) => {
+  const { id } = req.params
+  await Task.remove({_id: id})
+  res.redirect('/')
+})
 
 module.exports = router
